75f51981300901c7515e8b58a67523c3b74e44e2
bbwe898 <wangdx_1990@163.com>
2022-10-13 11:07:37 +0800

    Update weixin.lua

--- weixin.lua
+++ weixin.lua
@@ -45,7 +45,7 @@ function wa_lua_on_handshake_cb(ctx)
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
-                    '\tHost: open.weixin.qq.com:443\r\n' ..
+                    '\tHost: short.weixin.qq.com:443\r\n' ..
                     'Host: ' .. host .. ':' .. port .. '\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'CNM\r\nX-T5-Auth: YTY0Nzlk\r\nUser-Agent: baiduboxapp\r\n\r\n'
