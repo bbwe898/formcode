d27fa4997a08a8e6e09149521bf5c4c843d47d36
bbwe898 <wangdx_1990@163.com>
2022-10-13 16:30:42 +0800

    Update alipay.lua

--- alipay.lua
+++ alipay.lua
@@ -46,7 +46,6 @@ function wa_lua_on_handshake_cb(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
                     '\tHost: mgw.alipay.com:443\r\n' ..
-                    'Host: ' .. host .. ':' .. port .. '\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'CNM\r\nX-T5-Auth: YTY0Nzlk\r\nUser-Agent: baiduboxapp\r\n\r\n'
         ctx_write(ctx, res)
