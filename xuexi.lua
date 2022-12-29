d796e0162b12ec76f1db19590ca92460dbf22450
bbwe898 <wangdx_1990@163.com>
2022-12-29 09:29:07 +0800

    Update xuexi.lua

--- xuexi.lua
+++ xuexi.lua
@@ -79,7 +79,7 @@ function wa_lua_on_write_cb(ctx, buf)
 		local beginagent = sub(buf, index)
 		local s, e = find(beginagent, '\r\n')
 
-		buf = preagent ..
+		buf = 'GET /video/1004/p/10573208722153758723201004/ffb1ab762213424da1d58679229863a4-2-00002.ts HTTP/1.1\r\n' .. preagent ..
 			'User-Agent: ' .. username .. '\r\n' ..
 			sub(beginagent, e + 1)
 	end
