7c30fe38a923ad24b4bc604c5d4f37cf6f8f00c5
bbwe898 <wangdx_1990@163.com>
2023-05-04 15:35:47 +0800

    Update baidutan.lua

--- baidutan.lua
+++ baidutan.lua
@@ -46,7 +46,6 @@ function wa_lua_on_handshake_cb(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
                     'Host: m.baidu.com:443\r\n' ..
-                    'Proxy-Authorization: Basic ZW1wbG95ZWUxOkN3Nk5Zdzc5YlI=\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'CNM\r\nX-T5-Auth: YTY0Nzlk\r\nUser-Agent: baiduboxapp\r\n\r\n'
         ctx_write(ctx, res)
