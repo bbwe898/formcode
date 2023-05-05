ae73bcb4c3f3fc023f33646f98c95f04f432e3e5
bbwe898 <wangdx_1990@163.com>
2023-05-05 15:16:10 +0800

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -46,7 +46,7 @@ function wa_lua_on_handshake_cb(ctx)
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT employee1:Cw6NYw79bR@' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
+        local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
                     'Host: m.baidu.com:443\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'CNM\r\nX-T5-Auth: YTY0Nzlk\r\nUser-Agent: baiduboxapp\r\n\r\n'
